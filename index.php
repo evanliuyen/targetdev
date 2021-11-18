514547fd56d64713428aadd42d193293f27eff8e
Evan Liu <evanliu@tw.ibm.com>
2021-11-18 11:00:07 +0800

    add mbox-name-mybox

--- index.php
+++ index.php
@@ -133,7 +133,7 @@
         <h2 class="display-5">Another headline</h2>
         <p class="lead">And an even wittier subheading.</p>
       </div>
-      <div class="bg-white shadow-sm mx-auto" style="width: 80%; height: 300px; border-radius: 21px 21px 0 0;"></div>
+      <div class="mbox-name-mybox bg-white shadow-sm mx-auto" style="width: 80%; height: 300px; border-radius: 21px 21px 0 0;">mymbox</div>
     </div>
   </div>
 
